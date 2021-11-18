9363df9cbb6b74755c3df7ae2f9a81d9a522db12
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-11-18 21:36:09 +0530

    Add files via upload

---
+++ Junit.java
@@ -0,0 +1,31 @@
+package javapractise;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class Junit {
+	@Test
+	public void retriveDataFromDB()
+	{
+		EmployeePayRollService employeePayRollService=new EmployeePayRollService();
+		List<Employee> ep = employeePayRollService.retriveData();
+		Assert.assertEquals(6, ep.size());
+	}
+	@Test
+	public void updateSalaryFromDB()
+	{
+		EmployeePayRollService employeePayRollService=new EmployeePayRollService();
+		List<Employee> ep = employeePayRollService.retriveData();
+	    employeePayRollService.update();
+		
+	}
+	@Test
+	public void updateSalaryFromDBusing_prepareStatement() {
+		EmployeePayRollService employeePayRollService=new EmployeePayRollService();
+		employeePayRollService.updateUsing_PreparedStatement("charlie");
+	}
+}
