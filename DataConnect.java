8dd642470f7b03bb0f25603178107490ae1c16d6
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-11-18 12:36:46 +0530

    Connect To Database

--- DataConnect.java
+++ DataConnect.java
@@ -9,33 +9,32 @@ import java.util.Enumeration;
 public class DataConnect {
       
 	   
-	       public static void main(String[] args) {
+	       public static void main(String[] args)
+	       {
 			
-		
-	   // public static Connection createC() {
-		    String jdbcurl = "jdbc:mysql://localhost:3306/payroll_services";
+	                String jdbcurl = "jdbc:mysql://localhost:3306/payroll_services";
 			String username="root";
-		    String passWord="Tanaybr7614";
-		    Connection con;
+		        String passWord="Tanaybr7614";
+		        Connection con;
 		
 		try {
 				Class.forName("com.mysql.jdbc.Driver");
 				System.out.println("Driver loaded successfully");
 				con=DriverManager.getConnection(jdbcurl, username,passWord);
 				System.out.println("Connection is succesful "+con);
-		} catch (Exception e)
+		    } catch (Exception e)
+		    {
+		      e.printStackTrace();
+		    }
+		
+		    listdriver();
+		
+               }
+		private static void listdriver()
 		{
-				e.printStackTrace();
-		}
-		
-		
-		listdriver();
-//		return con;
-		
-	}
-		private static void listdriver() {
 			Enumeration<Driver> driverList=DriverManager.getDrivers();
-			while(driverList.hasMoreElements()) {
+			while(driverList.hasMoreElements())
+			{
 				Driver driverClass=driverList.nextElement();
 				System.out.println(" "+driverClass.getClass().getName());
 			}
