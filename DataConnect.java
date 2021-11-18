937bfa1846c938c0047d0a5a9c3a6f1907e995e6
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-11-18 16:08:25 +0530

    Add files via upload

---
+++ DataConnect.java
@@ -0,0 +1,41 @@
+package javapractise;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+public class DataConnect {
+      
+	        public static Connection createC() {
+		    String jdbcurl = "jdbc:mysql://localhost:3306/payroll_services";
+			String username="root";
+		    String passWord="Tanaybr7614";
+		    Connection con = null;
+		
+		try {
+				Class.forName("com.mysql.jdbc.Driver");
+				System.out.println("Driver loaded successfully");
+				con=DriverManager.getConnection(jdbcurl, username,passWord);
+				System.out.println("Connection is succesful "+con);
+		    } catch (Exception e)
+		    {
+				e.printStackTrace();
+		    }
+		
+		
+		listdriver();
+		return con;
+		
+	}
+		private static void listdriver()
+		{
+			Enumeration<Driver> driverList=DriverManager.getDrivers();
+			while(driverList.hasMoreElements()) 
+			{
+				Driver driverClass=driverList.nextElement();
+				System.out.println(" "+driverClass.getClass().getName());
+			}
+		}
+}
---
+++ Junit.java
@@ -0,0 +1,25 @@
+package javapractise;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class Junit {
+	@Test
+	public void retriveDataFromDB()
+	{
+		EmployeePayRollService employeePayRollService=new EmployeePayRollService();
+		List<Employee> ep = employeePayRollService.retriveData();
+		Assert.assertEquals(6, ep.size());
+	}
+//	@Test
+//	public void updateSalaryFromDB()
+//	{
+//		EmployeePayRollService employeePayRollService=new EmployeePayRollService();
+//	    employeePayRollService.update();
+//		
+//	}
+}
