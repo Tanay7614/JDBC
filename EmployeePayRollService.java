3eb8f1f93b3b4137bac2c9cd6d75ae198c37cd45
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-11-18 17:37:03 +0530

    Add files via upload

---
+++ EmployeePayRollService.java
@@ -0,0 +1,61 @@
+package javapractise;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeePayRollService {
+	
+	 List< Employee> list=new ArrayList<>();
+
+	public List<Employee> retriveData() 
+	{
+	  
+	   String sql="select * from employee_payrolls";
+	  
+	   try {
+		   Connection con1 = DataConnect.createC();
+		   Statement  statement=con1.createStatement();
+		   ResultSet result=statement.executeQuery(sql);
+		  
+		   while( result.next())
+		   {
+			   int id=result.getInt("id");
+			   String name=result.getString("name");
+			   String gender=result.getString("gender");
+			   double basic_pay=result.getDouble("basic_pay");
+			   LocalDate start=result.getDate("start").toLocalDate();
+			   list.add(new Employee(id,name,gender,basic_pay,start));
+			   		 
+		   }
+		   con1.close();
+	} catch (SQLException e) {
+		
+		e.printStackTrace();
+	}
+	  return list;
+	   
+	}
+
+	public void update()
+	{
+		String sql="update employee_payrolls set basic_pay=600000 where name='charlie'";
+		Connection con1 = DataConnect.createC();
+		try {
+			Statement statement=con1.createStatement();
+			statement.executeUpdate(sql);
+			System.out.println(list);	
+			con1.close();
+		} 
+		
+		catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+}
---
+++ Junit.java
@@ -0,0 +1,26 @@
+package javapractise;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class Junit {
+	@Test
+	public void retriveDataFromDB()
+	{
+		EmployeePayRollService employeePayRollService=new EmployeePayRollService();
+		List<Employee> ep = employeePayRollService.retriveData();
+		Assert.assertEquals(6, ep.size());
+	}
+	@Test
+	public void updateSalaryFromDB()
+	{
+		EmployeePayRollService employeePayRollService=new EmployeePayRollService();
+		List<Employee> ep = employeePayRollService.retriveData();
+	    employeePayRollService.update();
+		
+	}
+}
