439e5dbb38c46664828a50c55baa4e3ecde10027
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-11-18 16:20:22 +0530

    Update DataConnect.java

--- DataConnect.java
+++ DataConnect.java
@@ -4,15 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DataConnect {
-      
-	    static Connection con;
-	    
-	    public static Connection createC() {
+public class DataConnect
+{
+            static Connection con;
+	    public static Connection createC() 
+	    {
 		    String jdbcurl = "jdbc:mysql://localhost:3306/payroll_services";
-			String username="root";
-											
-		   String passWord="Tanaybr7614";
+		    String username="root";
+		    String passWord="Tanaybr7614";
 		
 		try {
 				Class.forName("com.mysql.jdbc.Driver");
@@ -20,11 +19,11 @@ public class DataConnect {
 				con=DriverManager.getConnection(jdbcurl, username, passWord);
 				System.out.println("connection is successfull "+con);
 				
-		} catch (Exception e)
-		{
-				e.printStackTrace();
-		}
-		return con;
+		    } catch (Exception e)
+		    {
+		                e.printStackTrace();
+		    }
+		    return con;
 		
-	}
+	    }
 }
