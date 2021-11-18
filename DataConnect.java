f7d219b57bf6a8973e75fdb9b72fbd8300888bf0
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-11-18 17:52:27 +0530

    Connect To Databases

--- DataConnect.java
+++ DataConnect.java
@@ -8,26 +8,25 @@ import java.util.Enumeration;
 
 public class DataConnect
 {
-      
-	        public static Connection createC()
+       public static Connection createC()
 	        {
 		           String jdbcurl = "jdbc:mysql://localhost:3306/payroll_services";
-			       String username="root";
+			   String username="root";
 		           String passWord="Tanaybr7614";
 		           Connection con = null;
 		
-		    try {
+		           try {
 				    Class.forName("com.mysql.jdbc.Driver");
 				    System.out.println("Driver loaded successfully");
 				    con=DriverManager.getConnection(jdbcurl, username,passWord);
 				    System.out.println("Connection is succesful "+con);
-		        } catch (Exception e)
-		        {
-			    	e.printStackTrace();
-		        }
+		               }    catch (Exception e)
+		               {
+			    	    e.printStackTrace();
+		               }
 		
-		        listdriver();
-		        return con;
+		                    listdriver();
+		                    return con;
 		
 	        }
 		private static void listdriver()
